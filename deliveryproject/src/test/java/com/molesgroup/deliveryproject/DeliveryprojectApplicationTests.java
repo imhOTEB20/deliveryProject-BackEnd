91ac52aec1bcb5e21ac68452955c235244e22917
imhOTEB <crisalbertoaa20@gmail.com>
2025-01-15 14:31:08 -0300

    primer commit

---
+++ deliveryproject/src/test/java/com/molesgroup/deliveryproject/DeliveryprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molesgroup.deliveryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
